8585d7abcbcfe41c28a7477e14bd2eea4b6c038c
Josh <joshuaj004@gmail.com>
2016-12-18 00:56:22 -0800

    Currently have the conversion to t9 working. Will try to implement trie soon.

---
+++ HW5/t9.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void file_handler(char* file_name);
+char* word_convert(char* word);
+
+int main(int argc, char** argv) {
+  file_handler(argv[1]);
+  return 0;
+}
+
+void file_handler(char* file_name) {
+  FILE* file = fopen(file_name, "r");
+  char line[500];
+
+  while (fgets(line, sizeof(line), file)) {
+    char* t9_word = word_convert(line);
+    printf("%s%s\n\n", line, t9_word);
+  }
+
+  fclose(file);
+}
+
+char* word_convert(char* word) {
+  char* t9_word = malloc(strlen(word) * sizeof(char));
+  for (int i = 0; i < strlen(word); i++) {
+    if (word[i] <= 'c') {
+      t9_word[i] = '2';
+    } else if (word[i] <= 'f') {
+      t9_word[i] = '3';
+    } else if (word[i] <= 'i') {
+      t9_word[i] = '4';
+    } else if (word[i] <= 'l') {
+      t9_word[i] = '5';
+    } else if (word[i] <= 'o') {
+      t9_word[i] = '6';
+    } else if (word[i] <= 's') {
+      t9_word[i] = '7';
+    } else if (word[i] <= 'v') {
+      t9_word[i] = '8';
+    } else if (word[i] <= 'z') {
+      t9_word[i] = '9';
+    } else {
+      // non-alphabet char
+    }
+  }
+  t9_word[strlen(word) - 1] = '\0';
+
+  return t9_word;
+}
+
